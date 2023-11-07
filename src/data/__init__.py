d485a92ade6f75db07d2d6fe39df2dc9d692571f
Hug0b27 <hugo.boucher1@ecoles-epsi.net>
2023-11-07 17:06:24 +0100

    OK

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,31 +1,37 @@
 import os
 import requests
-from datetime import datetime
+import pandas as pd
 
-# URL du répertoire contenant les fichiers
-url = 'https://exemple.com/chemin_du_dossier/'
+# URL du répertoire contenant les fichiers .parquet
+base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
 
-# Répertoire local de destination
-local_directory = '/votre_chemin_local/'
+# Répertoire local de destination pour les fichiers .parquet
+local_directory = '../../data/raw'
 
-# Années de début et de fin de la plage souhaitée
-start_year = 2018
-end_year = 2023
+# Créer un répertoire local s'il n'existe pas
+if not os.path.exists(local_directory):
+    os.makedirs(local_directory)
 
-# Fonction pour télécharger des fichiers dans une plage d'années
-def download_files_between_years(url, local_dir, start_year, end_year):
-    for year in range(start_year, end_year + 1):
+# Dates de début et de fin de la plage souhaitée
+start_date = (2018, 1)
+end_date = (2023, 8)
+
+# Fonction pour télécharger des fichiers .parquet pour une plage de dates
+def download_parquet_files_between_dates(base_url, local_dir, start_date, end_date):
+    for year in range(start_date[0], end_date[0] + 1):
         for month in range(1, 13):
+            if (year, month) < start_date or (year, month) > end_date:
+                continue
             file_name = f'yellow_tripdata_{year}_{month:02d}.parquet'
-            file_url = url + file_name
+            file_url = base_url + file_name
             local_file_path = os.path.join(local_dir, file_name)
 
-            response = requests.get(file_url, stream=True)
-            if response.status_code == 200:
+            with requests.get(file_url, stream=True) as r:
+                r.raise_for_status()
                 with open(local_file_path, 'wb') as local_file:
-                    for chunk in response.iter_content(chunk_size=8192):
+                    for chunk in r.iter_content(chunk_size=8192):
                         local_file.write(chunk)
                 print(f'Téléchargé : {file_name}')
 
-# Télécharger les fichiers entre les années de début et de fin spécifiées
-download_files_between_years(url, local_directory, start_year, end_year)
+# Télécharger les fichiers .parquet pour la plage de dates spécifiée
+download_parquet_files_between_dates(base_url, local_directory, start_date, end_date)
--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -1,7 +1,9 @@
 from minio import Minio
-import urllib.request
 import pandas as pd
 import sys
+import requests
+from urllib import request
+import os
 
 def main():
     grab_data()
@@ -11,10 +13,34 @@ def grab_data() -> None:
     """Grab the data from New York Yellow Taxi
 
     This method download x files of the New York Yellow Taxi. 
-    
+
     Files need to be saved into "../../data/raw" folder
     This methods takes no arguments and returns nothing.
     """
+
+    # Constants
+    base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata"
+    folder = "../../data/raw/"
+
+    year_range = range(2018, 2024)
+    month_range = range(1, 13)
+
+    for year in year_range:
+        error_count = 0
+        print(f"Downloading files for year {year}")
+        for month in month_range:
+            month_str = str(month).zfill(2)
+            url = f"{base_url}_{year}-{month_str}.parquet"
+            filename = os.path.join(folder, url.split('/')[-1])
+            print(f"Downloading file from {url} to {filename}")
+            try:
+                requests.urlretrieve(url, filename)
+                print(f"Downloaded file: {filename}")
+            except Exception as e:
+                break
+        print(f"Downloaded all files for year {year} !")
+
+    print("Downloaded all files !")
 
 
 def write_data_minio():
