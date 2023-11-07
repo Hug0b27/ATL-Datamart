b2da1e67adbf428067a8de5d212a4393c8a8295c
Hug0b27 <hugo.boucher1@ecoles-epsi.net>
2023-11-07 17:17:43 +0100

    telechargement de tous les fichiers OK

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -1,19 +1,14 @@
-from minio import Minio
-import pandas as pd
-import sys
-import requests
 from urllib import request
 import os
 
 def main():
     grab_data()
-    
 
 def grab_data() -> None:
     """Grab the data from New York Yellow Taxi
 
     This method download x files of the New York Yellow Taxi. 
-
+    
     Files need to be saved into "../../data/raw" folder
     This methods takes no arguments and returns nothing.
     """
@@ -30,11 +25,11 @@ def grab_data() -> None:
         print(f"Downloading files for year {year}")
         for month in month_range:
             month_str = str(month).zfill(2)
-            url = f"{base_url}_{year}-{month_str}.parquet"
+            url = f"{base_url}_{year}-{month_str}.parquet"  # Correction ici : changement de baseurl à base_url
             filename = os.path.join(folder, url.split('/')[-1])
             print(f"Downloading file from {url} to {filename}")
             try:
-                requests.urlretrieve(url, filename)
+                request.urlretrieve(url, filename)  # Correction ici : changement de requests à request
                 print(f"Downloaded file: {filename}")
             except Exception as e:
                 break
@@ -42,24 +37,5 @@ def grab_data() -> None:
 
     print("Downloaded all files !")
 
-
-def write_data_minio():
-    """
-    This method put all Parquet files into Minio
-    Ne pas faire cette méthode pour le moment
-    """
-    client = Minio(
-        "localhost:9000",
-        secure=False,
-        access_key="minio",
-        secret_key="minio123"
-    )
-    bucket: str = "NOM_DU_BUCKET_ICI"
-    found = client.bucket_exists(bucket)
-    if not found:
-        client.make_bucket(bucket)
-    else:
-        print("Bucket " + bucket + " existe déjà")
-
-if __name__ == '__main__':
-    sys.exit(main())
+if __name__ == '__main__':  # Correction ici : changement de name à __name__
+    main()
